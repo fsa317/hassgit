d6d3b37913525669486d19d5206693ad8313ecf1
fsa317 <fsa317@gmail.com>
2018-12-04 23:08:18 +0000

    minor changes

--- gitupdate.sh
+++ gitupdate.sh
@@ -12,3 +12,11 @@ cp /etc/systemd/system/home-assistant@homeassistant.service systemd/
 cp /etc/nginx/sites-enabled/default nginx/
 
 cp ~/key_commands.txt .
+
+cd ~/hassgit
+git add .
+git status
+echo -n "Enter the Description for the Change: " [Minor Update]
+read CHANGE_MSG
+git commit -m "${CHANGE_MSG}"
+git push origin master
