b5d1c5f6b5da1a79e3f33edd61ebd67109968726
fsa317 <fsa317@gmail.com>
2018-12-05 00:38:15 +0000

    more updates

--- gitupdate.sh
+++ gitupdate.sh
@@ -5,7 +5,7 @@ HASSHOME=/home/homeassistant/.homeassistant
 cp $HASSHOME/*.yaml hass/
 cp $HASSHOME/config/* hass/config/
 cp $HASSHOME/apps/* hass/apps/
-
+cp -r $HASSHOME/www hass/
 cp /etc/systemd/system/appdaemon@homeassistant.service systemd/
 cp /etc/systemd/system/home-assistant@homeassistant.service systemd/
 
