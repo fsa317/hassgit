82a87c391061c3e6f5c009fb8f6f373575cd7de5
fsa317 <fsa317@gmail.com>
2018-12-05 04:11:24 +0000

    auto update

---
+++ gitupdate_auto.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+HASSHOME=/home/homeassistant/.homeassistant
+
+cp $HASSHOME/*.yaml hass/
+cp $HASSHOME/config/* hass/config/
+cp $HASSHOME/apps/* hass/apps/
+cp -r $HASSHOME/www hass/
+cp /etc/systemd/system/appdaemon@homeassistant.service systemd/
+cp /etc/systemd/system/home-assistant@homeassistant.service systemd/
+
+cp /etc/nginx/sites-enabled/default nginx/
+
+cp ~/key_commands.txt .
+
+cd ~/hassgit
+git add .
+git status
+git commit -m "auto update"
+git push origin master
